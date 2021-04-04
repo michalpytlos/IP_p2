6242ad4433d8d1c15df4029854025e57b98c5ba8
Michal Pytlos <m.pytlos@devnet.de>
2021-04-04 21:15:05 +0200

    Add tests for quote engine

---
+++ QuoteEngine/__init__.py
@@ -0,0 +1,6 @@
+from .csv_ingestor import CSVIngestor
+from .pdf_ingestor import PDFIngestor
+from .docx_ingestor import DocxIngestor
+from .text_ingestor import TextIngestor
+from .ingestor import Ingestor
+from .quote_model import Quote
---
+++ tests/test_quote_engine.py
@@ -0,0 +1,43 @@
+import os
+
+from QuoteEngine import CSVIngestor, PDFIngestor, DocxIngestor, TextIngestor, Ingestor, Quote
+
+
+TEST_FILE_DIR = os.path.join('tests', 'SimpleLines')
+TEST_CSV = os.path.join(TEST_FILE_DIR, 'SimpleLines.csv')
+TEST_PDF = os.path.join(TEST_FILE_DIR, 'SimpleLines.pdf')
+TEST_TXT = os.path.join(TEST_FILE_DIR, 'SimpleLines.txt')
+TEST_DOCX = os.path.join(TEST_FILE_DIR, 'SimpleLines.docx')
+TEST_QUOTES = [Quote('Line 1', 'Author 1'),
+               Quote('Line 2', 'Author 2'),
+               Quote('Line 3', 'Author 3'),
+               Quote('Line 4', 'Author 4'),
+               Quote('Line 5', 'Author 5')]
+
+
+def test_csv_ingestor_can_ingest_csv():
+    assert CSVIngestor.can_ingest('file.csv')
+
+
+def test_csv_ingestor_can_ingest_non_csv():
+    assert not CSVIngestor.can_ingest('file.pdf')
+
+
+def test_csv_ingestor_parse():
+    assert CSVIngestor.parse(TEST_CSV) == TEST_QUOTES
+
+
+def test_pdf_ingestor_parse():
+    assert PDFIngestor.parse(TEST_PDF) == TEST_QUOTES
+
+
+def test_docx_ingestor_parse():
+    assert DocxIngestor.parse(TEST_DOCX) == TEST_QUOTES
+
+
+def test_text_ingestor_parse():
+    assert TextIngestor.parse(TEST_TXT) == TEST_QUOTES
+
+
+def test_ingestor_parse_text():
+    assert Ingestor.parse(TEST_TXT) == TEST_QUOTES
