b6b36fa00f91c20d002382d2ef38917766a2448d
Michal Pytlos <m.pytlos@devnet.de>
2021-04-04 21:15:38 +0200

    Implement quote model

--- QuoteEngine/quote_model.py
+++ QuoteEngine/quote_model.py
@@ -1,4 +1,14 @@
 class Quote:
 
     def __init__(self, body: str, author: str):
-        pass
+        self.body = body.strip('"')
+        self.author = author
+
+    def __eq__(self, other):
+        return self.body == other.body and self.author == other.author
+
+    def __repr__(self):
+        return f'Quote(body={self.body}, author={self.author})'
+
+    def __str__(self):
+        return f'"{self.body}" - {self.author}'
