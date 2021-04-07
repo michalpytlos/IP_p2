9e9e487282eb542f558a9dd3e23980360e8e3a5e
Michal Pytlos <m.pytlos@devnet.de>
2021-04-07 12:13:58 +0200

    Add meme generator module

---
+++ MemeGenerator/meme_generator.py
@@ -0,0 +1,56 @@
+from PIL import Image, ImageFont, ImageDraw
+import os
+from random import randint
+import textwrap
+
+
+class MemeEngine:
+
+    allowed_extensions = ['jpg', 'png']
+
+    def __init__(self, output_dir: str,
+                 text_font_path='./_data/fonts/Blinker/Blinker-Regular.ttf',
+                 author_font_path='./_data/fonts/Blinker/Blinker-Light.ttf'):
+        self.output_dir = output_dir
+        self.text_font_path = text_font_path
+        self.author_font_path = author_font_path
+
+    def make_meme(self, img_path: str, text: str, author: str, width=500,
+                  text_font_size=32, author_font_size=20) -> str:
+        file_extension = img_path.split('.')[-1].lower()
+        if file_extension not in self.allowed_extensions:
+            raise ValueError(f'Unable to open {img_path}. Allowed file types: {self.allowed_extensions}.')
+
+        with Image.open(img_path) as image:
+            # resize
+            orig_width, orig_height = image.size
+            height = round(width / orig_width * orig_height)
+            image = image.resize((width, height))
+
+            # add text
+            text_font = ImageFont.truetype(self.text_font_path, text_font_size)
+            author_font = ImageFont.truetype(self.author_font_path, author_font_size)
+            d = ImageDraw.Draw(image)
+            text_length, text_height = d.textsize(text, font=text_font)
+            author_length, author_height = d.textsize(author, font=author_font)
+            extra_padding = 0
+            if text_length >= width:
+                pixels_per_char = text_length / len(text)
+                max_line_length = round(width * 0.8 / pixels_per_char)
+                text = textwrap.fill(text, max_line_length)
+                text_length, text_height = d.textsize(text, font=text_font)
+                extra_padding = round(text_font_size * 0.3)
+
+            text_x = randint(0, width - text_length)
+            text_y = randint(0, max(0, height - text_height - author_height - extra_padding))
+            author_y = text_y + text_height + extra_padding
+            d.text((text_x, text_y), text, (0, 0, 0), font=text_font,
+                   stroke_width=2, stroke_fill=(255, 255, 255))
+            author = 4 * ' ' + '- ' + author
+            d.text((text_x, author_y), author, (0, 0, 0), font=author_font,
+                   stroke_width=1, stroke_fill=(255, 255, 255))
+
+            # save
+            out_path = os.path.join(self.output_dir, f'meme_{randint(0, 10000)}.{file_extension}')
+            image.save(out_path)
+        return out_path
