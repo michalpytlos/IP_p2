79dd07dded580b52e1711f634778b446784cf02a
Michal Pytlos <pollux.pw@gmail.com>
2021-04-09 19:49:46 +0200

    Refactor MemeEngine.
    * Make the code more modular.
    * Add more comments to the code.
    * Minor tweaks improving aesthetics of the memes.

--- MemeGenerator/meme_generator.py
+++ MemeGenerator/meme_generator.py
@@ -11,7 +11,7 @@ class MemeEngine:
 
     def __init__(self, output_dir: str,
                  text_font_path='./_data/fonts/Blinker/Blinker-Regular.ttf',
-                 author_font_path='./_data/fonts/Blinker/Blinker-Light.ttf'):
+                 author_font_path='./_data/fonts/Blinker/Blinker-Regular.ttf'):
         """Create a new 'MemeEngine'.
 
         :param output_dir: Path to the directory memes are saved to.
@@ -34,40 +34,77 @@ class MemeEngine:
         :param author_font_size: Font size of the author's name.
         :return: Path to the created meme.
         """
+        # Check file type
         file_extension = img_path.split('.')[-1].lower()
         if file_extension not in self.allowed_extensions:
             raise ValueError(f'Unable to open {img_path}. Allowed file types: {self.allowed_extensions}.')
 
         with Image.open(img_path) as image:
             # resize
-            orig_width, orig_height = image.size
-            height = round(width / orig_width * orig_height)
-            image = image.resize((width, height))
+            image = self.resize_image(image, width)
 
-            # add text
-            text_font = ImageFont.truetype(self.text_font_path, text_font_size)
-            author_font = ImageFont.truetype(self.author_font_path, author_font_size)
-            d = ImageDraw.Draw(image)
-            text_length, text_height = d.textsize(text, font=text_font)
-            author_length, author_height = d.textsize(author, font=author_font)
-            extra_padding = 0
-            if text_length >= width:
-                pixels_per_char = text_length / len(text)
-                max_line_length = round(width * 0.8 / pixels_per_char)
-                text = textwrap.fill(text, max_line_length)
-                text_length, text_height = d.textsize(text, font=text_font)
-                extra_padding = round(text_font_size * 0.3)
-
-            text_x = randint(0, width - text_length)
-            text_y = randint(0, max(0, height - text_height - author_height - extra_padding))
-            author_y = text_y + text_height + extra_padding
-            d.text((text_x, text_y), text, (0, 0, 0), font=text_font,
-                   stroke_width=2, stroke_fill=(255, 255, 255))
-            author = 4 * ' ' + '- ' + author
-            d.text((text_x, author_y), author, (0, 0, 0), font=author_font,
-                   stroke_width=1, stroke_fill=(255, 255, 255))
+            # add quote
+            self.add_quote(image, text, author, text_font_size, author_font_size)
 
             # save
             out_path = os.path.join(self.output_dir, f'meme_{randint(0, 10000)}.{file_extension}')
             image.save(out_path)
         return out_path
+
+    @staticmethod
+    def resize_image(image: Image.Image, width: int) -> Image.Image:
+        """Resize an image maintaining its aspect ratio.
+
+        :param image: Image.
+        :param width: Target width in pixels.
+        :return: Resized image.
+        """
+        orig_width, orig_height = image.size
+        height = round(width / orig_width * orig_height)
+        return image.resize((width, height))
+
+    def add_quote(self, image: Image.Image, text: str, author: str,
+                  text_font_size: int = 32, author_font_size: int = 20):
+        """Add a quote to an image.
+
+        :param image: Image.
+        :param text: Body of the quote.
+        :param author: Author of the quote.
+        :param text_font_size: Font size of the body of the quote.
+        :param author_font_size: Font size of the author's name.
+        """
+        width, height = image.size
+
+        # create fonts
+        text_font = ImageFont.truetype(self.text_font_path, text_font_size)
+        author_font = ImageFont.truetype(self.author_font_path, author_font_size)
+
+        # create an object for drawing on the image
+        d = ImageDraw.Draw(image)
+
+        # size caption
+        text_length, text_height = d.textsize(text, font=text_font)
+        author_length, author_height = d.textsize(author, font=author_font)
+        extra_padding = 0
+
+        # wrap text if it is too long
+        if text_length >= 0.8 * width:
+            pixels_per_char = text_length / len(text)
+            max_line_length = round(width * 0.8 / pixels_per_char)
+            text = textwrap.fill(text, max_line_length)
+            text_length, text_height = d.textsize(text, font=text_font)
+            extra_padding = round(text_font_size * 0.4)
+
+        # randomly select position of text
+        text_x = randint(0, width - text_length)
+        text_y = randint(0, max(0, height - text_height - author_height - extra_padding))
+        author_y = text_y + text_height + extra_padding
+
+        # add body of quote
+        d.text((text_x, text_y), text, (0, 0, 0), font=text_font,
+               stroke_width=2, stroke_fill=(255, 255, 255))
+
+        # add author
+        author = 4 * ' ' + '- ' + author
+        d.text((text_x, author_y), author, (0, 0, 0), font=author_font,
+               stroke_width=1, stroke_fill=(255, 255, 255))
